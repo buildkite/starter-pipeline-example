1c1f6e71831585ef393246a9a358b5f48bd0a776
Priya Kandababu <priyakandababu@192-168-1-122.tpgi.com.au>
2025-03-18 13:53:00 +1100

    test sign

---
+++ src/upload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo "uploading artifact" 
+
+local pipeline_file
+pipeline_file=$(mktemp --suffix .yaml --tmpdir pipeline.XXXXXXXXXX)
+# shellcheck disable=SC2064
+trap "rm -rf \"${pipeline_file}\"" EXIT
+local yaml_file
+yaml_file="$("$@")"
+
+if [[ -n "${yaml_file}" ]]; then
+
+  echo "${yaml_file}" > "${pipeline_file}"
+  buildkite-agent artifact upload "${pipeline_file}"
+  buildkite-agent pipeline upload "${pipeline_file}"
+fi
+
