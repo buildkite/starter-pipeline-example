df2969e789db8527cdea924c949ed4bcb6a84fc4
Priya Kandababu <priyakandababu@192-168-1-122.tpgi.com.au>
2025-03-18 14:03:49 +1100

    test sign

--- src/upload.sh
+++ src/upload.sh
@@ -4,19 +4,10 @@ set -euo pipefail  # Strict error handling
 
 echo "Uploading artifact..." 
 
-if [[ $# -ne 1 ]]; then
-  echo "Usage: $0 <yaml-file>" >&2
-  exit 1
-fi
 
-input_file="$1"
-
-if [[ ! -f "$input_file" ]]; then
-  echo "Error: File '$input_file' not found." >&2
-  exit 1
-fi
-
-pipeline_file=$(mktemp --suffix .yaml --tmpdir pipeline.XXXXXXXXXX)
+pwd
+ls -la
+pipeline_file= pipeline_upload.yml
 # shellcheck disable=SC2064
 trap 'rm -f "${pipeline_file}"' EXIT  # Ensure cleanup on exit
 
