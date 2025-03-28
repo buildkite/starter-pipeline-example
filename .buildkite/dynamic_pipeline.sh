229b06f61226ba6c0b53482ea0028d33c0bdd445
Priya Kandababu <priyakandababu@192-168-1-112.tpgi.com.au>
2025-03-28 15:46:59 +1100

    bump github.com/redis/go-redis/v9 from 9.7.0 to 9.7.3 (#9052)
    Bumps [github.com/redis/go-redis/v9](https://github.com/redis/go-redis) from 9.7.0 to 9.7.3.
    
    - [Release notes](https://github.com/redis/go-redis/releases)
    
    - [Changelog](https://github.com/redis/go-redis/blob/master/CHANGELOG.md)
    
    - [Commits](https://github.com/redis/go-redis/compare/v9.7.0...v9.7.3)
    
    ---
    
    updated-dependencies:
    
    - dependency-name: github.com/redis/go-redis/v9
    
      dependency-type: indirect
    
    ...

--- .buildkite/dynamic_pipeline.sh
+++ .buildkite/dynamic_pipeline.sh
@@ -8,10 +8,18 @@ echo "steps:"
 # A deploy step only if it's the master branch
 
 if [[ "$BUILDKITE_BRANCH" == "main" ]]; then
-  echo "  - wait"
-  echo "  - command: \"echo Deploy!\""  
-  echo "    label: \":rocket:\""
-  echo "  - command: \"echo test!\""  
-  echo "    label: \":test:\""
+  cat << EOF
+
+
+steps:
+  - label: ":rocket: Trigger Pipeline [baseimage]"
+    env:
+      AUTHELIA_RELEASE: "${BUILDKITE_MESSAGE}"
+      BUILDKITE_PULL_REQUEST: "${BUILDKITE_PULL_REQUEST}"
+      BUILDKITE_PULL_REQUEST_BASE_BRANCH: "${BUILDKITE_PULL_REQUEST_BASE_BRANCH}"
+      BUILDKITE_PULL_REQUEST_REPO: "${BUILDKITE_PULL_REQUEST_REPO}"
+    command: echo "hellp"
+
+EOF
 
 fi
