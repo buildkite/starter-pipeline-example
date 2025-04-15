7d7b999b438cc0e586ea1365a33f6c74b93c1a96
Priya Kandababu <priyakandababu@192-168-1-112.tpgi.com.au>
2025-04-15 11:55:53 +1000

    :test tf

--- main.tf
+++ main.tf
@@ -15,20 +15,17 @@ provider "buildkite" {
 
 data "buildkite_pipeline" "pipeline" {
   slug       = "child-pipeline"
-  
-  
+   
 }
 
-resource "buildkite_team" "team" {
-  name                = "Everyone"
-  privacy             = "VISIBLE"
-  default_team        = false
-  default_member_role = "MEMBER"
+data "buildkite_team" "team" {
+  slug = "Everyone"
 }
+
 
 # allow everyone in the "Everyone" team read-only access to pipeline
 resource "buildkite_pipeline_team" "pipeline_team" {
-  pipeline_id  = buildkite_pipeline.pipeline.id
-  team_id      = buildkite_team.team.id
+  pipeline_id  = data.buildkite_pipeline.pipeline.id
+  team_id      = data.buildkite_team.team.id
   access_level = "READ_ONLY"
 }
