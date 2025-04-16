831bb916865e158c69784b44b6e1f78879395228
Priya Kandababu <priyakandababu@192-168-1-112.tpgi.com.au>
2025-04-16 11:17:45 +1000

    skip commit

--- src/skip_commit.sh
+++ src/skip_commit.sh
@@ -15,16 +15,14 @@ echo "🔍 Checking for existing builds for commit $COMMIT..."
 RESPONSE=$(curl -s -H "Authorization: Bearer ${buildkite_api_token}" \
   "https://api.buildkite.com/v2/organizations/${BUILDKITE_ORGANIZATION_SLUG}/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds?commit=${COMMIT}")
 
+echo "Response ${RESPONSE}"
+
 # Count the number of occurrences of the "number" field in the JSON response
 BUILD_COUNT=$(echo "$RESPONSE" | grep -o '"number":' | wc -l)
 
 if [[ "$BUILD_COUNT" -gt 0 ]]; then
   echo "✅ Commit $COMMIT has already been built. Skipping step..."
-    buildkite-agent annotate \
-    --style "info" \
-    --context "skip_commit" \
-    --message "Skipping build for commit $COMMIT as it has already been built." \
-    "${BUILDKITE_BUILD_ID}"
+    buildkite-agent annotate  "Skipping build for commit $COMMIT as it has already been built." 
   exit 1
 else
   echo "🚀 No previous build found for commit $COMMIT. Proceeding..."
