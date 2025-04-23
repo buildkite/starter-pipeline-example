e8d4a8ee5b1d54fe59dba1caa2088e051e735a22
Priya Kandababu <priyakandababu@priyas-MacBook-Pro.local>
2025-04-23 14:06:43 +1000

    Test dyno

--- .buildkite/dynamic_pipeline.sh
+++ .buildkite/dynamic_pipeline.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-
 set -euo pipefail
-
-
-
-
 
 cat <<YAML
 steps:
   - label: "trigger"
-    id: trigger
-      cat <<EOF | buildkite-agent pipeline upload
-        steps:
-          - label: blah
-            depends_on: ~
-            command: echo hello
-      EOF
-    sleep 10
-      
+    command:
+      - cat <<EOF | buildkite-agent pipeline upload
+steps:
+  - label: "blah"
+    command: echo hello
+EOF
   - label: "finisher"
     depends_on: trigger
     command: echo "i am done"
 YAML
-    
